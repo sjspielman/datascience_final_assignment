e04ef0e71f83358daab9bba12c016e561632e1b3
Matt Petruncio <myrmecolacidae@gmail.com>
2020-04-30 15:11:55 +0000

    adding named repository

---
+++ petruncio_matt/app.R
@@ -0,0 +1,97 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+# Load all libraries ------------------------------------------------------------------------
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(colourpicker)
+
+source("covid_data_load.R") ## This line runs the Rscript "covid_data_load.R", which is expected to be in the same directory as this shiny app file!
+# The variables defined in `covid_data_load.R` are how fully accessible in this shiny app script!!
+
+# UI --------------------------------
+ui <- shinyUI(
+        navbarPage( theme = shinytheme("flatly"), ### Uncomment the theme and choose your own favorite theme from these options: https://rstudio.github.io/shinythemes/
+                   title = "YOUR VERY INTERESTING TITLE", ### Replace title with something reasonable
+            
+            ## All UI for NYT goes in here:
+            tabPanel("NYT data visualization", ## do not change this name
+            
+                    # All user-provided input for NYT goes in here:
+                    sidebarPanel(
+                        
+                        colourpicker::colourInput("nyt_color_cases", "Color for plotting COVID cases:", value = "blue"),
+                        colourpicker::colourInput("nyt_color_deaths", "Color for plotting COVID deaths:", value = "red")
+                        
+                    ), # closes NYT sidebarPanel. Note: we DO need a comma here, since the next line opens a new function     
+                    
+                    # All output for NYT goes in here:
+                    mainPanel(
+                        plotOutput("nyt_plot")
+                    ) # closes NYT mainPanel. Note: we DO NOT use a comma here, since the next line closes a previous function  
+            ), # closes tabPanel for NYT data
+            
+            
+            ## All UI for JHU goes in here:
+            tabPanel("JHU data visualization", ## do not change this name
+                     
+                     # All user-provided input for JHU goes in here:
+                     sidebarPanel(
+
+                         colourpicker::colourInput("jhu_color_cases", "Color for plotting COVID cases:", value = "purple"),
+                         colourpicker::colourInput("jhu_color_deaths", "Color for plotting COVID deaths:", value = "orange")
+                         
+                     ), # closes JHU sidebarPanel     
+                     
+                     # All output for JHU goes in here:
+                     mainPanel(
+                        plotOutput("jhu_plot")
+                     ) # closes JHU mainPanel     
+            ) # closes tabPanel for JHU data
+    ) # closes navbarPage
+) # closes shinyUI
+
+# Server --------------------------------
+server <- function(input, output, session) {
+
+    ## PROTIP!! Don't forget, all reactives and outputs are enclosed in ({}). Not just parantheses or curly braces, but BOTH! Parentheses on the outside.
+    
+    
+
+    
+    ## All server logic for NYT goes here ------------------------------------------
+    
+    ## Define a reactive for subsetting the NYT data
+    nyt_data <- reactive({})
+    
+    ## Define your renderPlot({}) for NYT panel that plots the reactive variable. ALL PLOTTING logic goes here.
+    nyt_plot <- renderPlot({})
+    
+    
+    
+    
+    ## All server logic for JHU goes here ------------------------------------------
+
+    
+    ## Define a reactive for subsetting the JHU data
+    jhu_data <- reactive({})
+    
+    ## Define your renderPlot({}) for JHU panel that plots the reactive variable. ALL PLOTTING logic goes here.
+    jhu_plot <- renderPlot({})
+    
+}
+
+
+
+
+
+# Do not touch below this line! ----------------------------------
+shinyApp(ui = ui, server = server)
