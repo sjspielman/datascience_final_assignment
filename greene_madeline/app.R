3f9edaec3a1256acc23079bf7ff8e2c1a38fbbcc
Madeline Greene <greenem6@students.rowan.edu>
2020-04-30 15:09:08 +0000

    Following Dr. Spielman 4/29

--- greene_madeline/app.R
+++ greene_madeline/app.R
@@ -73,7 +73,17 @@ server <- function(input, output, session) {
     nyt_data <- reactive({})
     
     ## Define your renderPlot({}) for NYT panel that plots the reactive variable. ALL PLOTTING logic goes here.
-    nyt_plot <- renderPlot({})
+    nyt_plot <- renderPlot({
+        nyt_data %>%
+            group_by(date, county, covid_type) %>%
+            summarise(total_county_day = sum(cumulative_number)) %>%
+            ggplot(aes(x = date, y = total_county_day, color = covid_type, group = covid_type)) +
+                geom_point() +
+                geom_line() +
+                scale_color_manual = c(input$nyt_color_cases, input$nyt_color_deaths)
+        
+        
+    })
     
     
     
