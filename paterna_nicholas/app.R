50ef1408b6a25c621e5eb31a7b86e390597c58c6
Nicholas Paterna <nickpaterna@Nicks-MBP.fios-router.home>
2020-05-05 12:36:12 -0400

    Finished JHU requirements

--- paterna_nicholas/app.R
+++ paterna_nicholas/app.R
@@ -19,94 +19,110 @@ source("covid_data_load.R") ## This line runs the Rscript "covid_data_load.R", w
 
 # UI --------------------------------
 ui <- shinyUI(
-        navbarPage(theme = shinytheme("sandstone"), ### Uncomment the theme and choose your own favorite theme from these options: https://rstudio.github.io/shinythemes/
-                   title = "YOUR VERY INTERESTING TITLE", ### Replace title with something reasonable
-            
-            ## All UI for NYT goes in here:
-            tabPanel("NYT data visualization", ## do not change this name
-            
-                    # All user-provided input for NYT goes in here:
-                    sidebarPanel(
+    navbarPage(theme = shinytheme("sandstone"), 
+               title = "Visualizing COVID-19",
+               
+               ## All UI for NYT goes in here:
+               tabPanel("NYT data visualization", ## do not change this name
                         
-                        colourpicker::colourInput("nyt_color_cases",
-                                            "Color for plotting COVID cases:",
-                                            value = "#A62D45"),
+                        # All user-provided input for NYT goes in here:
+                        sidebarPanel(
+                            
+                            colourpicker::colourInput("nyt_color_cases",
+                                                      "Color for plotting COVID cases:",
+                                                      value = "#A62D45"),
+                            
+                            colourpicker::colourInput("nyt_color_deaths",
+                                                      "Color for plotting COVID deaths:",
+                                                      value = "#495BE3"),
+                            
+                            selectInput("which_state",
+                                        "Which state would you like to plot?",
+                                        choices = usa_states,
+                                        selected = "Alabama"), ## input$which_state
+                            radioButtons("facet_county",
+                                         "Show counties across panels?",
+                                         choices = c("No", "Yes"),
+                                         selected = "No"),
+                            
+                            radioButtons("y_scale",
+                                         "Scale for Y-Axis?",
+                                         choices = c("Linear", "Log"),
+                                         selected = "Linear"),
+                            
+                            selectInput("which_theme",
+                                        "Choose a ggplot theme to use:",
+                                        choices = c("Black & White",
+                                                    "Classic",
+                                                    "Dark",
+                                                    "Minimal"),
+                                        selected = "Classic") ## input$which_theme
+                            
+                        ), # closes NYT sidebarPanel. Note: we DO need a comma here, since the next line opens a new function     
                         
-                        colourpicker::colourInput("nyt_color_deaths",
-                                            "Color for plotting COVID deaths:",
-                                            value = "#495BE3"),
+                        # All output for NYT goes in here:
+                        mainPanel(
+                            plotOutput("nyt_plot", height = "650px")
+                        ) # closes NYT mainPanel. Note: we DO NOT use a comma here, since the next line closes a previous function  
+               ), # closes tabPanel for NYT data
+               
+               
+               ## All UI for JHU goes in here:
+               tabPanel("JHU data visualization", ## do not change this name
                         
-                        selectInput("which_state",
-                                    "Which state would you like to plot?",
-                                    choices = usa_states,
-                                    selected = "Alabama"), ## input$which_state
-                       radioButtons("facet_county",
-                                    "Show counties across panels?",
-                                    choices = c("No", "Yes"),
-                                    selected = "No"),
-                         
-                        radioButtons("y_scale",
-                                     "Scale for Y-Axis?",
-                                     choices = c("Linear", "Log"),
-                                     selected = "Linear"),
+                        # All user-provided input for JHU goes in here:
+                        sidebarPanel(
+                            
+                            colourpicker::colourInput("jhu_color_cases",
+                                                      "Color for plotting COVID cases:",
+                                                      value = "#E09F12"),
+                            
+                            colourpicker::colourInput("jhu_color_deaths",
+                                                      "Color for plotting COVID deaths:",
+                                                      value = "#7D35BD"),
+                            
+                            selectInput("which_country",
+                                        "Which country or region would you like to plot?",
+                                        choices = world_countries_regions,
+                                        selected = "Afghanistan"),
+                            
+                            radioButtons("jhu_y_scale",
+                                         "Scale for Y-Axis?",
+                                         choices = c("Linear", "Log"),
+                                         selected = "Linear"),
+                            
+                            selectInput("jhu_theme",
+                                        "Choose a ggplot theme to use:",
+                                        choices = c("Black & White",
+                                                    "Classic",
+                                                    "Dark",
+                                                    "Minimal"),
+                                        selected = "Classic")
+                            
+                        ), # closes JHU sidebarPanel     
                         
-                        selectInput("which_theme",
-                                    "Which ggplot theme to use?",
-                                    choices = c("Classic", "Minimal"),
-                                    selected = "Classic") ## input$which_theme
-                        
-                    ), # closes NYT sidebarPanel. Note: we DO need a comma here, since the next line opens a new function     
-                    
-                    # All output for NYT goes in here:
-                    mainPanel(
-                        plotOutput("nyt_plot", height = "650px")
-                    ) # closes NYT mainPanel. Note: we DO NOT use a comma here, since the next line closes a previous function  
-            ), # closes tabPanel for NYT data
-            
-            
-            ## All UI for JHU goes in here:
-            tabPanel("JHU data visualization", ## do not change this name
-                     
-                     # All user-provided input for JHU goes in here:
-                     sidebarPanel(
-
-                         colourpicker::colourInput("jhu_color_cases",
-                                                   "Color for plotting COVID cases:",
-                                                   value = "purple"),
-                         
-                         colourpicker::colourInput("jhu_color_deaths",
-                                                   "Color for plotting COVID deaths:",
-                                                   value = "orange"),
-                         
-                         selectInput("which_country",
-                                     "Which country or region would you like to plot?",
-                                     choices = world_countries_regions,
-                                     selected = "Afghanistan")
-                         
-                     ), # closes JHU sidebarPanel     
-                     
-                     # All output for JHU goes in here:
-                     mainPanel(
-                        plotOutput("jhu_plot")
-                     ) # closes JHU mainPanel     
-            ) # closes tabPanel for JHU data
+                        # All output for JHU goes in here:
+                        mainPanel(
+                            plotOutput("jhu_plot", height = "500px")
+                        ) # closes JHU mainPanel     
+               ) # closes tabPanel for JHU data
     ) # closes navbarPage
 ) # closes shinyUI
 
 # Server --------------------------------
 server <- function(input, output, session) {
-
+    
     ## PROTIP!! Don't forget, all reactives and outputs are enclosed in ({}). Not just parantheses or curly braces, but BOTH! Parentheses on the outside.
     
     
-
     
-    ## All server logic for NYT goes here ------------------------------------------
+    
+    ## All server logic for NYT goes here -------------------------------------
     
     ## Define a reactive for subsetting the NYT data
     nyt_data_subset <- reactive({  
-       
-         nyt_data %>%
+        
+        nyt_data %>%
             filter(state == input$which_state) -> nyt_state
         
         if (input$facet_county == "No"){
@@ -123,7 +139,7 @@ server <- function(input, output, session) {
         
         final_nyt_state
         
-            })
+    })
     
     ## Define your renderPlot({}) for NYT panel that plots the reactive variable. ALL PLOTTING logic goes here.
     output$nyt_plot <- renderPlot({
@@ -132,30 +148,32 @@ server <- function(input, output, session) {
                        y = y,
                        color = covid_type,
                        group = covid_type)) +
-                geom_point() + 
-                geom_line() +
-                scale_color_manual(values = c(
-                                     input$nyt_color_cases,
-                                     input$nyt_color_deaths)) +
+            geom_point() + 
+            geom_line() +
+            scale_color_manual(values = c(
+                input$nyt_color_cases,
+                input$nyt_color_deaths)) +
             labs(title = paste(input$which_state, "Cases & Deaths"),
-                               x = "Date",
-                               y = "Cumulative Count",
-                               color = "Type") -> my_nyt_plot
+                 x = "Date",
+                 y = "Cumulative Count",
+                 color = "Type") -> myplot
         
         ## Deal with input$y_scale choice
         if (input$y_scale == "Log"){
-            my_nyt_plot <- my_nyt_plot + scale_y_log10()
+            myplot <- myplot + scale_y_log10()
         }
         
         ## Deal with input$facet_county
-        if (input$facet_county == "Yes") my_nyt_plot <- my_nyt_plot + facet_wrap(~county, scales = "free_y")
+        if (input$facet_county == "Yes") myplot <- myplot + facet_wrap(~county, scales = "free_y")
         
         ### Deal with input$which_theme choice
- if (input$which_theme == "Classic") my_nyt_plot <- my_nyt_plot + theme_classic()
- if (input$which_theme == "Minimal") my_nyt_plot <- my_nyt_plot + theme_minimal()
+        if (input$which_theme == "Black & White") myplot <- myplot + theme_bw()
+        if (input$which_theme == "Classic") myplot <- myplot + theme_classic()
+        if (input$which_theme == "Dark") myplot <- myplot + theme_dark()
+        if (input$which_theme == "Minimal") myplot <- myplot + theme_minimal()
         
         ## Return the plot to be plotted
-        my_nyt_plot + theme(axis.text = element_text(size = 12),
+        myplot + theme(axis.text = element_text(size = 12),
                        axis.title = element_text(size = 14,
                                                  face = "bold"),
                        plot.title = element_text(size = 20,
@@ -164,8 +182,7 @@ server <- function(input, output, session) {
     })
     
     
-    ## All server logic for JHU goes here ------------------------------------------
-
+    ## All server logic for JHU goes here -------------------------------------
     
     ## Define a reactive for subsetting the JHU data
     jhu_data_subset <- reactive({
@@ -193,14 +210,28 @@ server <- function(input, output, session) {
                  y = "Cumulative Count",
                  color = "Type") -> my_jhu_plot
         
-        my_jhu_plot
+        
+        ## Deal with input$y_scale choice
+        if (input$jhu_y_scale == "Log"){
+            my_jhu_plot <- my_jhu_plot + scale_y_log10()
+        }
+        
+        ## Deal with input$jhu_theme
+        if (input$jhu_theme == "Black & White") my_jhu_plot <- my_jhu_plot + theme_bw()
+        if (input$jhu_theme == "Classic") my_jhu_plot <- my_jhu_plot + theme_classic()
+        if (input$jhu_theme == "Dark") my_jhu_plot <- my_jhu_plot + theme_dark()
+        if (input$jhu_theme == "Minimal") my_jhu_plot <- my_jhu_plot + theme_minimal()
+        
+        ## Return the plot to be plotted
+        my_jhu_plot + theme(axis.text = element_text(size = 12),
+                       axis.title = element_text(size = 14,
+                                                 face = "bold"),
+                       plot.title = element_text(size = 20,
+                                                 face = "bold",
+                                                 hjust = 0.5))
     })
     
 }
-
-
-
-
 
 # Do not touch below this line! ----------------------------------
 shinyApp(ui = ui, server = server)
