7465db83452be807a6bb23037a25be3288303185
Paige Richards <paigerich@verizon.net>
2020-05-06 10:03:09 -0400

    modified the nyt_data and created all necessary widgets

--- richards_paige/app.R
+++ richards_paige/app.R
@@ -13,6 +13,7 @@ library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(colourpicker)
+themes_options <- c("Classic", "Minimal", "Grey", "Black and White", "Dark", "Light")
 
 source("covid_data_load.R") ## This line runs the Rscript "covid_data_load.R", which is expected to be in the same directory as this shiny app file!
 # The variables defined in `covid_data_load.R` are how fully accessible in this shiny app script!!
@@ -21,7 +22,7 @@ source("covid_data_load.R") ## This line runs the Rscript "covid_data_load.R", w
 # UI --------------------------------
 ui <- shinyUI(
         navbarPage(theme = shinytheme("cyborg"), ### Uncomment the theme and choose your own favorite theme from these options: https://rstudio.github.io/shinythemes/
-                   title = "YOUR VERY INTERESTING TITLE", ### Replace title with something reasonable
+                   title = "COVID-19 Shiny App", ### Replace title with something reasonable
             
             ## All UI for NYT goes in here:
             tabPanel("NYT data visualization", ## do not change this name
@@ -29,14 +30,28 @@ ui <- shinyUI(
                     # All user-provided input for NYT goes in here:
                     sidebarPanel(
                         
-                        colourpicker::colourInput("nyt_color_cases", "Color for plotting COVID cases:", value = "blue"),
-                        colourpicker::colourInput("nyt_color_deaths", "Color for plotting COVID deaths:", value = "red")
-                        
+                        colourpicker::colourInput("nyt_color_cases", "Color for plotting COVID cases:", value = "darkorchid"),
+                        colourpicker::colourInput("nyt_color_deaths", "Color for plotting COVID deaths:", value = "darkturquoise"), #need comma because we are putting another one
+                        selectInput("which_state", ## input$which_state,
+                                    "What state would you like to see COVID-19 data for?",
+                                    choices = usa_states,
+                                    selected = "Wyoming"), #chose this because there is a meme that Wyoming does not exist, closes selectInput
+                        radioButtons("facet_county",
+                                     "Do you want to see all the Counties of this State individually or all pooled together?",
+                                     choices = c("Individually", "Together"),
+                                     selected = "Together"),
+                        radioButtons("y_scale",
+                                     "What scale do you want to see for the Y axis?",
+                                     choices = c("Linear", "Log"),
+                                     selected = "Linear"), #closes radio button
+                        selectInput("which_theme",
+                                    "What theme are you interested in seeing?",
+                                    choices = themes_options)
                     ), # closes NYT sidebarPanel. Note: we DO need a comma here, since the next line opens a new function     
                     
                     # All output for NYT goes in here:
                     mainPanel(
-                        plotOutput("nyt_plot")
+                        plotOutput("nyt_plot", height = "1000px", width = "2000px")
                     ) # closes NYT mainPanel. Note: we DO NOT use a comma here, since the next line closes a previous function  
             ), # closes tabPanel for NYT data
             
@@ -47,8 +62,8 @@ ui <- shinyUI(
                      # All user-provided input for JHU goes in here:
                      sidebarPanel(
 
-                         colourpicker::colourInput("jhu_color_cases", "Color for plotting COVID cases:", value = "purple"),
-                         colourpicker::colourInput("jhu_color_deaths", "Color for plotting COVID deaths:", value = "orange")
+                         colourpicker::colourInput("jhu_color_cases", "Color for plotting COVID cases:", value = "lightseagreen"),
+                         colourpicker::colourInput("jhu_color_deaths", "Color for plotting COVID deaths:", value = "slateblue4")
                          
                      ), # closes JHU sidebarPanel     
                      
@@ -71,28 +86,68 @@ server <- function(input, output, session) {
     ## All server logic for NYT goes here ------------------------------------------
     
     ## Define a reactive for subsetting the NYT data
-    nyt_data <- reactive({})
+    nyt_data_subset <- reactive({nyt_data %>% #need to use this to make the data cleaner
+            filter(state == input$which_state) -> nyt_state
+        
+        if(input$facet_county == "Together"){
+            nyt_state %>% #can't have lines going down. and pooled county data together
+            group_by(date, covid_type) %>%
+            summarize(y = sum(cumulative_number)) -> final_nyt_state
+        }
+        
+        if(input$facet_county == "Individually"){
+            nyt_state %>%
+                rename(y = cumulative_number) -> final_nyt_state
+        }
+        
+        final_nyt_state
+        }) #this closes nyt_data_subset
     
     ## Define your renderPlot({}) for NYT panel that plots the reactive variable. ALL PLOTTING logic goes here.
-    nyt_plot <- renderPlot({
-        nyt_data %>%
-            filter(state == "Alabama") %>% #can't have lines going down. 
-            group_by(covid_type, date) %>% #needed to remove counties to get this into an appropriate line plot
-            summarize(total_county_day = sum(cumulative_number)) %>%
-            ggplot(aes(x = date, y = total_county_day, color = covid_type)) +
+    output$nyt_plot <- renderPlot({
+        nyt_data_subset() %>%
+            ggplot(aes(x = date, y = y, color = covid_type, group = covid_type)) +
                 geom_point() +
                 geom_line() +
-                scale_color_manual = c(input$nyt_color_cases, input$nyt_color_deaths)
+                scale_color_manual(values = c(input$nyt_color_cases, input$nyt_color_deaths)) +
+            labs(x = "Date",
+                 y = "Cumulative Count",
+                 color = "Covid Type",
+                 title = paste(input$which_state, "cases and deaths")) -> myploot
+        
+    ##If they decided to use log    
+       if ( input$y_scale == "Log"){
+           myploot <- myploot + scale_y_log10()
+       } #closes if statement
+    ##otherwise 
+    
+    ##Input for facet_county
+        if(input$facet_county == "Individually") myploot <- myploot + facet_wrap(~county)
+        
+    ##Which theme choice
+        if (input$which_theme == "Classic") myploot <- myploot + theme_classic()
+        if (input$which_theme == "Minimal") myploot <- myploot + theme_minimal()
+        if (input$which_theme == "Grey") myploot <- myploot + theme_grey()
+        if (input$which_theme == "Black and White") myploot <- myploot + theme_bw()
+        if (input$which_theme == "Dark") myploot <- myploot + theme_dark()
+        if (input$which_theme == "Light") myploot <- myploot + theme_light()
+        myploot + theme(axis.title = element_text(size = 15),
+                        axis.text = element_text(size = 12),
+                        plot.title = element_text(size = 30))
+        
     })
     
  
-    
+    #radioButtons("facet_county",
+                 #"Do you want to see all the Counties of this State individually or all pooled together?",
+                 #choices = c("Individually", "Together"),
+                 #selected = "Together")
     
     ## All server logic for JHU goes here ------------------------------------------
 
     
     ## Define a reactive for subsetting the JHU data
-    jhu_data <- reactive({})
+    jhu_data_subset <- reactive({})
     
     ## Define your renderPlot({}) for JHU panel that plots the reactive variable. ALL PLOTTING logic goes here.
     jhu_plot <- renderPlot({})
